7423fd98ba52508b51bba2284a27acea1047cc8c
TestVagrant-1 <TestVagrant-1@Ajay-Macbook-TestVagrant.local>
2024-02-05 15:33:58 +0530

    product class created, pantry screen started, products as list -- SUCCESS

---
+++ lib/classes/Products.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class Products {
+  final String name;
+  final String image;
+
+  Products(this.name, this.image);
+}
--- lib/pages/PantryDetailsScreen.dart
+++ lib/pages/PantryDetailsScreen.dart
@@ -1,16 +1,41 @@
+import 'package:first_project/classes/Products.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class PantryDetailsScreen extends StatelessWidget {
+
+  final List<Products> product = [
+    Products('Britania', 'assets/britaniaCake.jpeg'),
+    Products('GoodDay', 'assets/goodDay.jpg'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Pantry Details'),
       ),
-      body: Center(
-        child: Text('Details about the Pantry'),
-      ),
+      body: Column(
+          children: [
+            Expanded(
+                child: ListView.builder(
+                  itemCount: product.length,
+                    itemBuilder: (context, index)
+                {
+                  return ListTile(
+                    leading: Image.asset(product[index].image),
+                    title: Text(product[index].name),
+                    trailing: ElevatedButton(
+                      onPressed: (){
+        
+                      },
+                      child: Text('Add'),
+                    ),
+                  );
+                }),
+            ),
+          ],
+        ),
     );
   }
 }
