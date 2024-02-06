1ce5306c7fe083e1ce41091092367ead9b67be48
TestVagrant-1 <TestVagrant-1@Ajay-Macbook-TestVagrant.local>
2024-02-06 11:39:52 +0530

    gridview done for pantry screen

--- lib/pages/PantryDetailsScreen.dart
+++ lib/pages/PantryDetailsScreen.dart
@@ -7,6 +7,9 @@ class PantryDetailsScreen extends StatelessWidget {
   final List<Products> product = [
     Products('Britania', 'assets/britaniaCake.jpeg'),
     Products('GoodDay', 'assets/goodDay.jpg'),
+    Products('Namkeen', 'assets/tabletennis.jpg'),
+    Products('kodbule', 'assets/man.png'),
+    Products('Akshay', 'assets/man.png')
   ];
 
   @override
@@ -15,27 +18,53 @@ class PantryDetailsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Pantry Details'),
       ),
-      body: Column(
-          children: [
-            Expanded(
-                child: ListView.builder(
-                  itemCount: product.length,
-                    itemBuilder: (context, index)
-                {
-                  return ListTile(
-                    leading: Image.asset(product[index].image),
+      // body: Column(
+      //     children: [
+      //       Expanded(
+      //           child: ListView.builder(
+      //             itemCount: product.length,
+      //               itemBuilder: (context, index)
+      //           {
+      //             return ListTile(
+      //               leading: Image.asset(product[index].image),
+      //               title: Text(product[index].name),
+      //               trailing: ElevatedButton(
+      //                 onPressed: (){
+      //  
+      //                 },
+      //                 child: Text('Add'),
+      //               ),
+      //             );
+      //           }),
+      //       ),
+      //     ],
+      //   ),
+      body: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 8.0,
+            mainAxisSpacing: 45.0,
+          ),
+          itemCount: product.length,
+          itemBuilder: (context, index) {
+            return Card(
+              child: Column(
+                children: [
+                  Expanded(child: Image.asset(product[index].image)),
+                  ListTile(
                     title: Text(product[index].name),
                     trailing: ElevatedButton(
                       onPressed: (){
-        
-                      },
+
+                        },
                       child: Text('Add'),
                     ),
-                  );
-                }),
-            ),
-          ],
-        ),
+                  ),
+                ],
+              ),
+            );
+          }
+      ),
     );
   }
 }
